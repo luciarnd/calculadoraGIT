7f1a067e9efd36e89a06dfab6a1cdf0ae7284875
LuciaArnedo <daw02.2022@gmail.com>
2022-02-17 11:19:26 +0100

    Proyecto base

---
+++ src/calculadoras/calculadora.java
@@ -0,0 +1,10 @@
+package calculadoras;
+
+public class calculadora {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ src/calculadoras/principal.java
@@ -0,0 +1,10 @@
+package calculadoras;
+
+public class principal {
+
+	public static void main (String[] args) {
+		
+		calculadora mical = new calculadora();
+	}
+
+}
