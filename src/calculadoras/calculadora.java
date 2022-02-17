7d175163d1be88cf204c42bc75eb4c7c1ba66335
LuciaArnedo <daw02.2022@gmail.com>
2022-02-17 11:26:39 +0100

    Funcion resta

--- src/calculadoras/calculadora.java
+++ src/calculadoras/calculadora.java
@@ -2,8 +2,12 @@ package calculadoras;
 
 public class calculadora {
 
-	public int suma (int a, int b) {
+	public int suma(int a, int b) {
 		return a + b;
+	}
+
+	public int resta(int a, int b) {
+		return a - b;
 	}
 
 }
